0b7ff878a9420bc0e1bf94f980d6b8d1f7405e1b
Tuomas-Talkio <122274744+Tuomas-Talkio@users.noreply.github.com>
2023-09-11 09:29:01 +0300

    php test

---
+++ tehtava1.php
@@ -0,0 +1,24 @@
+<?php
+include ("header.html");
+?>
+
+<?php
+$x=23;
+$y=9;
+$z=15;
+
+$tulo=$x*$y*$z;
+
+print "\$x = $x";
+print "<br>"; //tulostukseen rivinvaihto 
+print "\$y = $y";
+print "<br>"; //tulostukseen rivinvaihto 
+print "\$z = $z";
+print "<br>"; //tulostukseen rivinvaihto
+print "Muuttujien tulo $tulo";
+
+?>
+
+<?php
+include ("footer.html");
+?>
